02e8453ffb3db1677b47faa953fcdb4185cb2c55
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-07 15:21:13 +0600

    change mac using python script linux enviroment

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -4,13 +4,18 @@ import optparse
 
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change mack address. Example- eth0, wlan0")
-parser.parse_args()
+parser.add_option("-m", "--mac", dest="set_mac", help="Set MAC address. Example '00:11:22:33:44:55'."
+                                                      "Make sure it will 12 characters")
+(options, argument) = parser.parse_args()
 
-interface = input("interface > ")
-set_mac = input("set Mac > ")
+interface = options.interface or input("interface > ")
+set_mac = options.set_mac or input("set Mac > ")
+
+# interface = input("interface > ")
+# set_mac = input("set Mac > ")
 
 subprocess.run(["ifconfig", interface, "down"])
 subprocess.run(["ifconfig", interface, "hw", "ether", set_mac])
 subprocess.run(["ifconfig", interface, "up"])
 
-print("[+] Change MAC address " + interface + "to" + set_mac)
+print("[+] Change MAC address " + interface + " to " + set_mac)
