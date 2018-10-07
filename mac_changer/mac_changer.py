b8581af7aa8f192ef13dde3ee5409d5ef236a092
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-07 14:35:29 +0600

    using list passing command

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -4,7 +4,12 @@ import subprocess
 interface = input("interface > ")
 set_mac = input("set Mac > ")
 
-subprocess.run("ifconfig " + interface + " down", shell=True, check=True)
-subprocess.run("ifconfig " + interface + " hw ether " + set_mac, shell=True, check=True)
-subprocess.run("ifconfig " + interface + " up", shell=True, check=True)
+# subprocess.run("ifconfig " + interface + " down", shell=True, check=True)
+# subprocess.run("ifconfig " + interface + " hw ether " + set_mac, shell=True, check=True)
+# subprocess.run("ifconfig " + interface + " up", shell=True, check=True)
 
+subprocess.run(["ifconfig", interface, "down"])
+subprocess.run(["ifconfig", interface, "hw", "ether", set_mac])
+subprocess.run(["ifconfig", interface, "up"])
+
+print("Change MAC address " + interface + "to" + set_mac)
