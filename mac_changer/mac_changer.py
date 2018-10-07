cd2356ceec9982313f03b7b6acb15cf44ef3b467
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-07 14:16:43 +0600

    change mac address in linux with py

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,6 +1,10 @@
 # !/user/bin/python3
 import subprocess
 
-subprocess.run("ifconfig eth0 down", shell=True, check=True)
-subprocess.run("ifconfig eth0 hw ether 00:99:33:77:22:55", shell=True, check=True)
-subprocess.run("ifconfig eth0 up", shell=True, check=True)
+interface = input("interface > ")
+set_mac = input("set Mac > ")
+
+subprocess.run("ifconfig " + interface + " down", shell=True, check=True)
+subprocess.run("ifconfig " + interface + " hw ether " + set_mac, shell=True, check=True)
+subprocess.run("ifconfig " + interface + " up", shell=True, check=True)
+
