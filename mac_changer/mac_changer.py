e06ecedabe79c41fc94e3f195f6558530e45cf37
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-08 20:50:52 +0600

    change manual code to function

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,18 +2,25 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change mack address. Example- eth0, wlan0")
-parser.add_option("-m", "--mac", dest="set_mac", help="Set MAC address. Example '00:11:22:33:44:55'."
-                                                      "Make sure it will 12 characters")
-(options, argument) = parser.parse_args()
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface to change mack address. Example- eth0, wlan0")
+    parser.add_option("-m", "--mac", dest="set_mac", help="Set MAC address. sure it will 12 characters")
+    return parser.parse_args()
+
+
+def change_mac(interface, set_mac):
+    subprocess.run(["ifconfig", interface, "down"])
+    subprocess.run(["ifconfig", interface, "hw", "ether", set_mac])
+    subprocess.run(["ifconfig", interface, "up"])
+
+    print("[+] Change MAC address " + interface + " to " + set_mac)
+
+
+(options, argument) = get_arguments()
 interface = options.interface or input("interface > ")
 set_mac = options.set_mac or input("set Mac > ")
 
-
-subprocess.run(["ifconfig", interface, "down"])
-subprocess.run(["ifconfig", interface, "hw", "ether", set_mac])
-subprocess.run(["ifconfig", interface, "up"])
-
-print("[+] Change MAC address " + interface + " to " + set_mac)
+change_mac(interface, set_mac)
