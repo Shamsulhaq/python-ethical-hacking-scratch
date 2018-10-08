7ccb7849006509386df9575478ce018f235ae0bf
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-08 21:05:51 +0600

    mac changer python script for linux enviroment

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -8,7 +8,12 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface",
                       help="Interface to change mack address. Example- eth0, wlan0")
     parser.add_option("-m", "--mac", dest="set_mac", help="Set MAC address. sure it will 12 characters")
-    return parser.parse_args()
+    (options, argument) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif options.set_mac:
+        parser.error("[-] Please specify new mac, use --help for more info")
+    return options
 
 
 def change_mac(interface, set_mac):
@@ -19,7 +24,7 @@ def change_mac(interface, set_mac):
     print("[+] Change MAC address " + interface + " to " + set_mac)
 
 
-(options, argument) = get_arguments()
+options = get_arguments()
 interface = options.interface or input("interface > ")
 set_mac = options.set_mac or input("set Mac > ")
 
