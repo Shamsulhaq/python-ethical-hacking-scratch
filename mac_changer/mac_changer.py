4e0c3878d61005d5b2d32d05f505201bc8b29c71
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-10 03:54:04 +0600

    bug fixing: elif option.set_mack to not options.set_mac

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -8,10 +8,11 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface",
                       help="Interface to change mack address. Example- eth0, wlan0")
     parser.add_option("-m", "--mac", dest="set_mac", help="Set MAC address. sure it will 12 characters")
+    # return parser.parse_args()
     (options, argument) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info")
-    elif options.set_mac:
+    elif not options.set_mac:
         parser.error("[-] Please specify new mac, use --help for more info")
     return options
 
@@ -25,7 +26,15 @@ def change_mac(interface, set_mac):
 
 
 options = get_arguments()
-interface = options.interface or input("interface > ")
-set_mac = options.set_mac or input("set Mac > ")
+# interface = options.interface or input("interface > ")
+# set_mac = options.set_mac or input("mack > ")
 
-change_mac(interface, set_mac)
+interface = options.interface
+set_mac = options.set_mac
+
+print([interface, set_mac])
+# change_mac(interface, set_mac)
+
+
+result_checking = subprocess.check_output(["ifconfig", options.interface], stderr=subprocess.STDOUT, shell=True)
+print(result_checking)
