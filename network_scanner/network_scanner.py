4e8f2c29e29baffb37f5313faae21117241a543e
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-18 18:07:00 +0600

    network scanner Output

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,13 +1,25 @@
 # !/user/bin/python
 from scapy import all as scapy
 
+"""
+    pdst = 
+    psrc = ip address
+    hwsrc = hardware / mac address
+"""
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_broadcast_request = broadcast/arp_request
-    print(arp_broadcast_request.summary())
-    arp_broadcast_request.show()
+    answered_list = scapy.srp(arp_broadcast_request, timeout=1, verbose=False)[0]
+
+    print('\nSCANNING RESULT')
+    print('-'*50, "\nIP\t\t\tMAC ADDRESS")
+    print('-'*50)
+
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 
 scan("192.168.31.1/24")
