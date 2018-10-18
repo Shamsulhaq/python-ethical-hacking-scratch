862f5c44baafbca11fc940893b4917d525e07887
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-18 15:40:39 +0600

    combiining frames to broadcast packets

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,7 +4,10 @@ from scapy import all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_broadcast_request = broadcast/arp_request
+    print(arp_broadcast_request.summary())
+    arp_broadcast_request.show()
 
 
 scan("192.168.31.1/24")
