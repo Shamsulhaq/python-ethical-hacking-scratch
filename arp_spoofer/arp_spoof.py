07c0430a92c6ee83fa32775742a18b33ae81db90
jakiiii <me.jaki@outlook.com>
2018-10-28 03:54:54 +0600

    resolve auto marge problem

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -6,8 +6,5 @@ paket = scapy.ARP(
     pdst="192.168.31.162",
     hwsrc="88:78:73:30:1C:95",
     psrc="192.168.31.1")
-<<<<<<< HEAD
-=======
 
 scapy.send(paket)
->>>>>>> bd80c1bbb0964f2f33cd9269e0bdf99b0b4b4cea
